15f91e9501f8c5954a1b46a6431ab1d1caef7a58
sspabhishek <abhishekprajapati243@gmail.com>
2025-02-06 18:34:37 +0530

    added routes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -27,8 +27,9 @@ const Footer = () => {
                         <li><Link to="/about" className="block px-4 py-2 lg:p-0 hover:text-blue-700" onClick={scrollToTop}>About us</Link></li>
                         <li><Link to="/practice-quiz" className='hover:text-blue-700' onClick={scrollToTop}>Practices</Link></li>
                         <li><Link to="/contact" className="block px-4 py-2 lg:p-0 hover:text-blue-700" onClick={scrollToTop}>Contact us</Link></li>
-                        <li><Link to="/add-practice-quiz" className='hover:text-blue-700' onClick={scrollToTop}>Term & Conditions</Link></li>
+                        <li><Link to="#" className='hover:text-blue-700' onClick={scrollToTop}>Term & Conditions</Link></li>
                         <li><Link to="#" className='hover:text-blue-700' onClick={scrollToTop}>Privacy Policy</Link></li>
+                        <li><Link to="/add-practice-quiz" className='hover:text-blue-700' onClick={scrollToTop}>Term & Conditions</Link></li>
                     </ul>
                 </div>
 
